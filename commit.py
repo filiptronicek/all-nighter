a1bf722732197e5197175dec3a0ce323eb8fe5d8
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-18 22:56:08 +0200

    Create commit.py

---
+++ commit.py
@@ -0,0 +1,14 @@
+from time import sleep
+import os
+
+def commit():
+    delay = 7200
+
+    os.system("git add .")
+
+    os.system('git commit -m "Logs update"')
+    os.system("git push")
+
+    sleep(delay)
+    commit()
+commit()
